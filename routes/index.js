1328498c216152cfc81cac2445728ef532350288
Soufiane Boutallaka <s.boutallaka@gmail.com>
2018-10-11 10:30:14 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/name', function(req, res, next) {
+  res.json({"name":"Soufiane"});
+});
+
+module.exports = router;
